6a249aa369df39f2a00b9ec20da2b55e9c9e251d
Clément Chaine <clement.chaine25@gmail.com>
2023-12-16 20:02:37 +0000

    Starts implementing the traceability report

--- src/ecap5_treq/main.py
+++ src/ecap5_treq/main.py
@@ -2,7 +2,7 @@ import argparse
 import os
 from req import extract_reqs 
 from check import Check, extract_checks, import_testdata
-from report import generate_test_report, generate_test_result_badge
+from report import generate_report_summary, generate_test_report, generate_traceability_report, generate_test_result_badge
 from matrix import import_matrix
 from config import load_config, check_config
 from analyse import analyse_tests
@@ -71,7 +71,12 @@ def cmd_gen_report(config, args):
     testdata = import_testdata(config["testdata_dir_path"])
     matrix = import_matrix(config["matrix_path"])
     analysis = analyse_tests(reqs, checks, testdata, matrix)
-    report = generate_test_report(*analysis)
+
+    report_summary = generate_report_summary(*analysis)
+    test_report = generate_test_report(*analysis)
+    traceability_report = generate_traceability_report(*analysis)
+    report = report_summary + test_report + traceability_report
+
     if(args.output):
         with open(args.output, 'w') as f:
             f.write(report)
--- src/ecap5_treq/report.py
+++ src/ecap5_treq/report.py
@@ -1,14 +1,26 @@
 import sys
 import os
 
-def generate_test_report(count_success, testsuites, no_testsuite, skipped_tests, unknown_tests, checks):
-    report = "# Test report\n"
+def generate_report_summary(count_success, testsuites, no_testsuite, skipped_tests, unknown_tests, checks):
+    testresult = int(count_success / len(checks) * 100.0)
+    traceabilityresult = 0
+
+    report = "# Summary\n"
     report += "<table>\n"
-    report += "<thead><tr><th>Success</th><th>Failure</th><th>Total</th></tr></thead>\n"
-    report += "<tr><td>{}</td><td>{}</td><td>{}</td></tr>\n".format(count_success, len(checks)-count_success, len(checks))
+    report += "<tr><td>{}</td><td><a href=\"#test-report\">Test report</a></td><td align=\"right\">{}</td>\n".format("✅" if testresult == 100 else "🚫", str(testresult) + "%")
+    report += "<tr><td>{}</td><td><a href=\"#traceability-report\">Traceability report</a></td><td align=\"right\">{}</td>\n".format("✅" if traceabilityresult == 100 else "🚫", str(traceabilityresult) + "%")
+    report += "</table>\n"
+
+    return report + "\n"
+
+def generate_test_report(count_success, testsuites, no_testsuite, skipped_tests, unknown_tests, checks):
+    report = "## Test report\n"
+    report += "<table>\n"
+    report += "<thead><tr><th></th><th>Success</th><th>Failure</th><th>Total</th></tr></thead>\n"
+    report += "<tr><td>Tests</td><td align=\"right\">{}</td><td align=\"right\">{}</td><td align=\"right\">{}</td></tr>\n".format(count_success, len(checks)-count_success, len(checks))
     report += "</table>\n\n"
 
-    report += "## Run tests\n"
+    report += "### Run tests\n"
     report += "<table>\n"
     report += "<thead><tr><th>Testsuite</th><th>Test check</th><th>Status</th><th>Log</th></tr></thead>\n"
     for t in testsuites:
@@ -16,28 +28,37 @@ def generate_test_report(count_success, testsuites, no_testsuite, skipped_tests,
             report += "<tr>"
             if i == 0:
                 report += "<td rowspan=\"{}\"><samp>{}</samp></td>".format(len(testsuites[t]), t)
-            report += "<td><samp>{}</samp></td><td>{}</td><td>{}</td></tr>\n".format(c.id, "✅" if c.status else "🚫", c.errormsg if c.errormsg else "")
+            report += "<td><samp>{}</samp></td><td align=\"center\">{}</td><td>{}</td></tr>\n".format(c.id, "✅" if c.status else "🚫", c.errormsg if c.errormsg else "")
     # Handle tests that do not belong to a testsuite
     for i, c in enumerate(no_testsuite):
         report += "<tr>"
         if i == 0:
             report += "<td rowspan=\"{}\"></td>".format(len(no_testsuite))
-        report += "<td><samp>{}</samp></td><td>{}</td><td>{}</td></tr>\n".format(c.id, "✅" if c.status else "🚫", c.errormsg if c.errormsg else "")
+        report += "<td><samp>{}</samp></td><td align=\"center\">{}</td><td>{}</td></tr>\n".format(c.id, "✅" if c.status else "🚫", c.errormsg if c.errormsg else "")
     report += "</table>\n\n"
 
     if(len(skipped_tests) > 0):
-        report += "## Skipped tests\n"
+        report += "### Skipped tests\n"
         report += "<table>\n"
         for t in skipped_tests:
             report += "<tr><td><samp>{}</samp></td></tr>\n".format(t.id)
         report += "</table>\n\n"
 
     if(len(unknown_tests) > 0):
-        report += "## Unknown tests\n"
+        report += "### Unknown tests\n"
         report += "<table>\n"
         for t in unknown_tests:
             report += "<tr><td><samp>{}</samp></td></tr>\n".format(t.id)
         report += "</table>\n\n"
+
+    return report
+
+def generate_traceability_report(count_success, testsuites, no_testsuite, skipped_tests, unknown_tests, checks):
+    report = "## Traceability report\n"
+    report += "<table>\n"
+    report += "<thead><tr><th></th><th>Covered</th><th>Untraceable</th><th>Uncovered</th><th>Total</th></tr></thead>\n"
+    report += "<tr><td>Requirements</td><td align=\"right\">{}</td><td align=\"right\">{}</td><td align=\"right\">{}</td><td align=\"right\">{}</td></tr>\n".format(0, 0, 0, len(checks))
+    report += "</table>\n\n"
 
     return report
 
