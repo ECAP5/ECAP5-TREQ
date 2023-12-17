524da9c65cc6d87b959af4d6c3af5d597abb3c88
Clément Chaine <clement.chaine25@gmail.com>
2023-12-17 09:44:43 +0000

    Adds quick links to sections of the report

--- src/ecap5_treq/report.py
+++ src/ecap5_treq/report.py
@@ -41,9 +41,9 @@ def generate_test_report(analysis):
     report += "  <tr>\n"
     report += "    <td>Tests</td>\n"
     report += "    <td align=\"right\">{}</td>\n".format(analysis.num_successfull_checks)
-    report += "    <td align=\"right\">{}</td>\n".format(analysis.num_failed_checks)
-    report += "    <td align=\"right\">{}</td>\n".format(len(analysis.skipped_checks))
-    report += "    <td align=\"right\">{}</td>\n".format(len(analysis.unknown_checks))
+    report += "    <td align=\"right\"><a href=\"#first-failed-check\">{}</a></td>\n".format(analysis.num_failed_checks)
+    report += "    <td align=\"right\"><a href=\"#skipped-checks\">{}</a></td>\n".format(len(analysis.skipped_checks))
+    report += "    <td align=\"right\"><a href=\"#unknown-checks\">{}</a></td>\n".format(len(analysis.unknown_checks))
     report += "    <td align=\"right\">{}</td>\n".format(len(analysis.checks))
     report += "  </tr>\n"
     report += "</table>\n"
@@ -58,8 +58,10 @@ def generate_test_report(analysis):
     report += "      <th>Log</th>\n"
     report += "    </tr>\n"
     report += "  </thead>\n"
+    failed_test_anchor = False
     for t in analysis.testsuites:
         for i, c in enumerate(analysis.testsuites[t]):
+            check_status_icon = "✅" if c.status else "🚫"
             report += "  <tr>\n"
             # Insert the name of the testsuite on the first row
             if i == 0:
@@ -69,7 +71,11 @@ def generate_test_report(analysis):
             report += "    <td>\n"
             report += "      <samp>{}</samp>\n".format(c.id)
             report += "    </td>\n"
-            report += "    <td align=\"center\">{}</td>\n".format("✅" if c.status else "🚫")
+            report += "    <td align=\"center\">\n"
+            if not c.status and not failed_test_anchor_placed:
+                report += "      <a id=\"first-failed-check\"></a>"
+            report += "      {}\n".format(check_status_icon)
+            report += "    </td>\n"
             report += "    <td>{}</td>\n".format(c.errormsg if c.errormsg else "")
             report += "  </tr>\n"
 
@@ -88,7 +94,7 @@ def generate_test_report(analysis):
     report += "</table>\n"
 
     if(len(analysis.skipped_checks) > 0):
-        report += "\n### Skipped tests\n"
+        report += "\n### <a id=\"skipped-checks\"></a> Skipped tests\n"
         report += "<table>\n"
         for t in analysis.skipped_checks:
             report += "  <tr>\n"
@@ -99,7 +105,7 @@ def generate_test_report(analysis):
         report += "</table>\n"
 
     if(len(analysis.unknown_checks) > 0):
-        report += "\n### Unknown tests\n"
+        report += "\n### <a id=\"unknown-checks\"></a> Unknown tests\n"
         report += "<table>\n"
         for t in analysis.unknown_checks:
             report += "  <tr>\n"
@@ -127,7 +133,7 @@ def generate_traceability_report(analysis):
     report += "    <td>Requirements</td>\n"
     report += "    <td align=\"right\">{}</td>\n".format(len(analysis.covered_reqs))
     report += "    <td align=\"right\">{}</td>\n".format(0)
-    report += "    <td align=\"right\">{}</td>\n".format(0)
+    report += "    <td align=\"right\"><a href=\"#uncovered-reqs\">{}</a></td>\n".format(0)
     report += "    <td align=\"right\">{}</td>\n".format(len(analysis.reqs))
     report += "  </tr>\n"
     report += "</table>\n"
