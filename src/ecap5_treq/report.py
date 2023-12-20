dff1bbcfd4dd10d03a63c8ab4b04caf42b96ade3
Clément Chaine <clement.chaine25@gmail.com>
2023-12-20 15:07:24 +0000

    Fix duplicate pourcent char in badges

--- src/ecap5_treq/report.py
+++ src/ecap5_treq/report.py
@@ -331,8 +331,8 @@ def generate_test_result_badge(analysis):
     badge = "{"
     badge += "\"schemaVersion\": 1, "
     badge += "\"label\": \"Test result\", "
-    badge += "\"message\": \"{}%\", ".format(str(analysis.test_result) + "%")
-    badge += "\"color\": \"hsl({}, 100%, 40%)\"".format(str(analysis.test_result))
+    badge += "\"message\": \"{}%\", ".format(analysis.test_result)
+    badge += "\"color\": \"hsl({}, 100%, 40%)\"".format(analysis.test_result)
     badge += "}"
     return badge
 
@@ -340,7 +340,7 @@ def generate_traceability_result_badge(analysis):
     badge = "{"
     badge += "\"schemaVersion\": 1, "
     badge += "\"label\": \"Traceability\", "
-    badge += "\"message\": \"{}%\", ".format(str(analysis.traceability_result) + "%")
-    badge += "\"color\": \"hsl({}, 100%, 40%)\"".format(str(analysis.traceability_result))
+    badge += "\"message\": \"{}%\", ".format(analysis.traceability_result)
+    badge += "\"color\": \"hsl({}, 100%, 40%)\"".format(analysis.traceability_result)
     badge += "}"
     return badge
