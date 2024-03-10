5fd6d3f70828b9b0b93fa035773aeab5d5fd6f9f
Clément Chaine <clement.chaine25@gmail.com>
2024-03-10 11:22:51 +0100

    Update the format of the traceability matrix for untraceable requirements (#63)
    Closes #49

--- src/ecap5_treq/tests/test_matrix.py
+++ src/ecap5_treq/tests/test_matrix.py
@@ -25,7 +25,7 @@ import pytest
 
 from ecap5_treq.matrix import Matrix, prepare_matrix
 from ecap5_treq.check import Check
-from ecap5_treq.log import log_error, log_clear
+from ecap5_treq.log import log_error, log_clear, log_warn
 
 #
 # Fixture definitions
@@ -64,6 +64,7 @@ def test_Matrix_read_01():
         matrix.read("path")
 
     assert len(matrix.data.keys()) == 0
+    assert len(matrix.untraceable.keys()) == 0
 
 def test_Matrix_read_02():
     """Unit test for the read method of the Matrix class
@@ -80,6 +81,7 @@ def test_Matrix_read_02():
     assert "element2" in matrix.data
     assert matrix.data["element1"] == ["content1"]
     assert matrix.data["element2"] == ["content2", "content3"]
+    assert matrix.untraceable == {}
 
 def test_Matrix_read_03():
     """Unit test for the read method of the Matrix class
@@ -96,6 +98,42 @@ def test_Matrix_read_03():
     assert "element2" in matrix.data
     assert matrix.data["element1"] == ["content1"]
     assert matrix.data["element2"] == []
+    assert matrix.untraceable == {}
+
+def test_Matrix_read_04():
+    """Unit test for the read method of the Matrix class
+
+    The covered behavior is a valid matrix with an untraceable requirement traced multiple times
+    """
+    empty_element_matrix = "element1;content1\nelement2\n__UNTRACEABLE__;req1;just1\n__UNTRACEABLE__;req1;just2\n"
+    with patch("builtins.open", mock_open(read_data=empty_element_matrix)):
+        matrix = Matrix()
+        matrix.read("path")
+
+    assert len(matrix.data.keys()) == 2
+    assert "element1" in matrix.data
+    assert "element2" in matrix.data
+    assert matrix.data["element1"] == ["content1"]
+    assert matrix.data["element2"] == []
+    assert matrix.untraceable == {"req1": "just2"}
+    assert len(log_warn.msgs) == 1
+
+def test_Matrix_read_05():
+    """Unit test for the read method of the Matrix class
+
+    The covered behavior is a valid matrix with an untraceable requirement with no justification
+    """
+    empty_element_matrix = "element1;content1\nelement2\n__UNTRACEABLE__;req1;just1\n__UNTRACEABLE__;req2\n"
+    with patch("builtins.open", mock_open(read_data=empty_element_matrix)):
+        matrix = Matrix()
+        matrix.read("path")
+
+    assert len(matrix.data.keys()) == 2
+    assert "element1" in matrix.data
+    assert "element2" in matrix.data
+    assert matrix.data["element1"] == ["content1"]
+    assert matrix.data["element2"] == []
+    assert matrix.untraceable == {"req1": "just1", "req2": ""}
 
 def test_Matrix_check_01():
     """Unit test for the check method of the Matrix class
@@ -126,7 +164,7 @@ def test_Matrix_check_02():
 def test_Matrix_check_03():
     """Unit test for the check method of the Matrix class
 
-    The covered behavior is a check equal to false due to the matrix missing the __UNTRACEABLE__ entry
+    The covered behavior is a check equal to true even if the matrix missing the __UNTRACEABLE__ entry
     """
     valid_matrix = "testsuite1.testcase1.element1;content1\ntestsuite1.testcase1.element2;content2;content3\n"
     with patch("builtins.open", mock_open(read_data=valid_matrix)):
@@ -134,7 +172,7 @@ def test_Matrix_check_03():
         matrix.read("path")
 
     checks = [Check("testsuite1", "testcase1", "element1"), Check("testsuite1", "testcase1", "element2")]
-    assert matrix.check(checks) == False
+    assert matrix.check(checks) == True
 
 def test_Matrix_add():
     """Unit test for the add method of the Matrix class
@@ -153,6 +191,7 @@ def test_Matrix_add():
     # Check that the element was properly added
     assert "added_element" in matrix.data
     assert matrix.data["added_element"] == ["content1", "content2"]
+    assert matrix.untraceable == {}
 
 def test_Matrix_get():
     """Unit test for the get method of the Matrix class
@@ -168,6 +207,24 @@ def test_Matrix_get():
 
     assert matrix.get("element1") == ["content1"]
     assert matrix.get("unknown") == []
+
+def test_Matrix_add_untraceable():
+    """Unit test for the add_untraceable method of the Matrix class
+    """
+    valid_matrix = "element1;content1\nelement2;content2;content3"
+    with patch("builtins.open", mock_open(read_data=valid_matrix)):
+        matrix = Matrix()
+        matrix.read("path")
+
+    # Check that the element was not present before
+    assert "added_element" not in matrix.untraceable
+
+    # Add the element
+    matrix.add_untraceable("added_element", "justification")
+
+    # Check that the element was properly added
+    assert "added_element" in matrix.untraceable
+    assert matrix.untraceable["added_element"] == "justification"
     
 def test_Matrix___contains__():
     """Unit test for the __contains__ method of the Matrix class
@@ -191,21 +248,28 @@ def test_Matrix___eq__():
     matrix.add("key1", "content1")
     matrix.add("key2", "content2")
     matrix.add("key3", "content3")
+    matrix.add_untraceable("req1", "just1")
 
     other = Matrix()
     other.add("key1", "content1")
     other.add("key2", "content2")
     other.add("key3", "content3")
+    other.add_untraceable("req1", "just1")
 
     assert matrix == other
 
     other.add("key4", "content4")
     assert matrix != other
+    
+    matrix.add("key4", "content4")
+    assert matrix == other
+    matrix.add_untraceable("req2", "just2")
+    assert matrix != other
 
 def test_Matrix_to_csv():
     """Unit test for the to_csv method of the Matrix class
     """
-    valid_matrix = "element1;content1\nelement2;content2;content3\n__UNTRACEABLE__"
+    valid_matrix = "element1;content1\nelement2;content2;content3\n__UNTRACEABLE__;req1;just1\n__UNTRACEABLE__;req2;just2\n"
     with patch("builtins.open", mock_open(read_data=valid_matrix)):
         base_matrix = Matrix()
         base_matrix.read("path")
@@ -218,11 +282,12 @@ def test_Matrix_to_csv():
 
     # Compare the two matrices
     assert base_matrix.data == new_matrix.data
+    assert base_matrix.untraceable == new_matrix.untraceable
 
 def test_Matrix___repr__():
     """Unit test for the __repr__ method of the Matrix class
     """
-    valid_matrix = "element1;content1\nelement2;content2;content3\n__UNTRACEABLE__"
+    valid_matrix = "element1;content1\nelement2;content2;content3\n__UNTRACEABLE__;req1;just1\n__UNTRACEABLE__;req2;just2\n"
     with patch("builtins.open", mock_open(read_data=valid_matrix)):
         base_matrix = Matrix()
         base_matrix.read("path")
@@ -235,11 +300,12 @@ def test_Matrix___repr__():
 
     # Compare the two matrices
     assert base_matrix.data == new_matrix.data
+    assert base_matrix.untraceable == new_matrix.untraceable
 
 def test_Matrix___str__():
     """Unit test for the __str__ method of the Matrix class
     """
-    valid_matrix = "element1;content1\nelement2;content2;content3\n__UNTRACEABLE__"
+    valid_matrix = "element1;content1\nelement2;content2;content3\n__UNTRACEABLE__;req1;just1\n__UNTRACEABLE__;req2;just2\n"
     with patch("builtins.open", mock_open(read_data=valid_matrix)):
         base_matrix = Matrix()
         base_matrix.read("path")
@@ -252,6 +318,7 @@ def test_Matrix___str__():
 
     # Compare the two matrices
     assert base_matrix.data == new_matrix.data
+    assert base_matrix.untraceable == new_matrix.untraceable
 
 #
 # Tests targetting functions in matrix module
@@ -265,13 +332,13 @@ def test_prepare_matrix_01():
     checks = [Check("testsuite1", "testcase1", "check1"), Check("testsuite1", "testcase1", "check2")]
     matrix = prepare_matrix(checks, None)
 
-    assert len(matrix.data) == 3
+    assert len(matrix.data) == 2
+    assert len(matrix.untraceable) == 0
     assert "testsuite1.testcase1.check1" in matrix.data
     assert "testsuite1.testcase1.check2" in matrix.data
-    assert "__UNTRACEABLE__" in matrix.data
     assert matrix.data["testsuite1.testcase1.check1"] == []
     assert matrix.data["testsuite1.testcase1.check2"] == []
-    assert matrix.data["__UNTRACEABLE__"] == []
+    assert matrix.untraceable == {}
 
 def test_prepare_matrix_02():
     """Unit test for the prepare_matrix function
@@ -282,13 +349,13 @@ def test_prepare_matrix_02():
     previous_matrix = Matrix()
     matrix = prepare_matrix(checks, previous_matrix)
 
-    assert len(matrix.data) == 3
+    assert len(matrix.data) == 2
+    assert len(matrix.untraceable) == 0
     assert "testsuite1.testcase1.check1" in matrix.data
     assert "testsuite1.testcase1.check2" in matrix.data
-    assert "__UNTRACEABLE__" in matrix.data
     assert matrix.data["testsuite1.testcase1.check1"] == []
     assert matrix.data["testsuite1.testcase1.check2"] == []
-    assert matrix.data["__UNTRACEABLE__"] == []
+    assert matrix.untraceable == {}
 
 def test_prepare_matrix_03():
     """Unit test for the prepare_matrix function
@@ -296,16 +363,19 @@ def test_prepare_matrix_03():
     The covered behavior is with a previous matrix containing one of the check and having an untraceable element
     """
     checks = [Check("testsuite1", "testcase1", "check1"), Check("testsuite1", "testcase1", "check2")]
-    valid_matrix = "testsuite1.testcase1.element1;content1\ntestsuite1.testcase1.check2;content2;content3\n__UNTRACEABLE__;content4"
+    valid_matrix = "testsuite1.testcase1.element1;content1\ntestsuite1.testcase1.check2;content2;content3\n__UNTRACEABLE__;req1;just1\n__UNTRACEABLE__;req2;just2\n"
     with patch("builtins.open", mock_open(read_data=valid_matrix)):
         previous_matrix = Matrix()
         previous_matrix.read("path")
     matrix = prepare_matrix(checks, previous_matrix)
 
-    assert len(matrix.data) == 3
+    assert len(matrix.data) == 2
+    assert len(matrix.untraceable) == 2
     assert "testsuite1.testcase1.check1" in matrix.data
     assert "testsuite1.testcase1.check2" in matrix.data
-    assert "__UNTRACEABLE__" in matrix.data
+    assert "req1" in matrix.untraceable
+    assert "req2" in matrix.untraceable
     assert matrix.data["testsuite1.testcase1.check1"] == []
     assert matrix.data["testsuite1.testcase1.check2"] == ["content2", "content3"]
-    assert matrix.data["__UNTRACEABLE__"] == ["content4"]
+    assert matrix.untraceable["req1"] == "just1"
+    assert matrix.untraceable["req2"] == "just2"
