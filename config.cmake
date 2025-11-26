7943b6070f307f86ca4e9c8ca90baa77ed904480
Clément Chaine <clement.chaine25@gmail.com>
2025-11-26 17:42:42 +0100

    Fix package name

--- config.cmake
+++ config.cmake
@@ -30,42 +30,42 @@
 #
 # The following variables shall be defined :
 #   
-#   ecap5_treq_EXECUTABLE : path to the ecap5-treq executable
+#   ecap5_ecap5_treq_EXECUTABLE : path to the ecap5-treq executable
 #
 # The following optional variables can be defined :
 #   
-#   ecap5_treq_CONFIG_PATH : path to the ecap5-treq configuration file
+#   ecap5_ecap5_treq_CONFIG_PATH : path to the ecap5-treq configuration file
 #
 ######################################################################
 
-if(DEFINED ecap5_treq_EXECUTABLE)
-  if(NOT DEFINED ecap5_treq_CONFIG_PATH)
-    set(ecap5_treq_CONFIG_PATH ${CMAKE_SOURCE_DIR}/config/treq.json)
+if(DEFINED ecap5_ecap5_treq_EXECUTABLE)
+  if(NOT DEFINED ecap5_ecap5_treq_CONFIG_PATH)
+    set(ecap5_ecap5_treq_CONFIG_PATH ${CMAKE_SOURCE_DIR}/config/treq.json)
   endif()
 
   # Define commands for using ECAP5-TREQ
   add_custom_target(report
     WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-    COMMAND ${ecap5_treq_EXECUTABLE} -c ${ecap5_treq_CONFIG_PATH} gen_report -o ${CMAKE_BINARY_DIR}/report.html --html)
+    COMMAND ${ecap5_ecap5_treq_EXECUTABLE} -c ${ecap5_ecap5_treq_CONFIG_PATH} gen_report -o ${CMAKE_BINARY_DIR}/report.html --html)
   add_custom_command(
     OUTPUT report.md
     WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-    COMMAND ${ecap5_treq_EXECUTABLE} -c ${ecap5_treq_CONFIG_PATH} gen_report -o ${CMAKE_BINARY_DIR}/report.md)
+    COMMAND ${ecap5_ecap5_treq_EXECUTABLE} -c ${ecap5_ecap5_treq_CONFIG_PATH} gen_report -o ${CMAKE_BINARY_DIR}/report.md)
   add_custom_target(report_markdown DEPENDS report.md)
   add_custom_command(
     OUTPUT test-result-badge.json
     WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-    COMMAND ${ecap5_treq_EXECUTABLE} -c ${ecap5_treq_CONFIG_PATH} gen_test_result_badge -o ${CMAKE_BINARY_DIR}/test-result-badge.json)
+    COMMAND ${ecap5_ecap5_treq_EXECUTABLE} -c ${ecap5_ecap5_treq_CONFIG_PATH} gen_test_result_badge -o ${CMAKE_BINARY_DIR}/test-result-badge.json)
   add_custom_command(
     OUTPUT traceability-result-badge.json
     WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-    COMMAND ${ecap5_treq_EXECUTABLE} -c ${ecap5_treq_CONFIG_PATH} gen_traceability_result_badge -o ${CMAKE_BINARY_DIR}/traceability-result-badge.json)
+    COMMAND ${ecap5_ecap5_treq_EXECUTABLE} -c ${ecap5_ecap5_treq_CONFIG_PATH} gen_traceability_result_badge -o ${CMAKE_BINARY_DIR}/traceability-result-badge.json)
   add_custom_target(badges DEPENDS test-result-badge.json traceability-result-badge.json)
 
   add_custom_command(
     OUTPUT traceability-matrix.csv
     WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-    COMMAND ${ecap5_treq_EXECUTABLE} -c ${ecap5_treq_CONFIG_PATH} prepare_matrix -o ${CMAKE_BINARY_DIR}/traceability-matrix.csv)
+    COMMAND ${ecap5_ecap5_treq_EXECUTABLE} -c ${ecap5_ecap5_treq_CONFIG_PATH} prepare_matrix -o ${CMAKE_BINARY_DIR}/traceability-matrix.csv)
   add_custom_target(prepare_matrix DEPENDS traceability-matrix.csv)
 else()
   message(WARNING "Could not find ECAP5-TREQ")
